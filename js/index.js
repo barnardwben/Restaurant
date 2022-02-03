419eb8a03bc8819be0d372cd9c4ef2cc5ad2ca2e
Benjamin Barnard <barnardwben@yahoo.com>
2022-02-03 22:43:43 +0100

    Testing branches

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function component() {
   const element = document.createElement("div");
 
-  element.innerHTML = "Hello World";
+  element.innerHTML = "Hello World Testing";
 
   return element;
 }
